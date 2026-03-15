2f10432c3fca095258af12568dca00974045ad49
xyq30 <1650813119@qq.com>
2026-03-16 02:15:36 +0800

    feat: 增加文段的解析与抽取功能，并提供了模拟上传；暂时为触发式，后期调好后可以改为自动提取上传

--- frontend 3.14/project/src/pages/MainPage.js
+++ frontend 3.14/project/src/pages/MainPage.js
@@ -7,7 +7,6 @@ import { renderFileList } from '../components/FileList.js';
 import { uploadFiles, executeCommand, getTask, getFields } from '../api/index.js';
 import { escapeHtml, toggleFullscreen } from '../utils/helpers.js';
 
-// 全局状态
 let fileArray = [];
 let currentAvatar = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'40\' height=\'40\' viewBox=\'0 0 24 24\' fill=\'%2394a3b8\'%3E%3Cpath d=\'M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z\'/%3E%3C/svg%3E';
 let currentName = '旅行者';
